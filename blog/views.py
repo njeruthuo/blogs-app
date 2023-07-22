b1f7341e423a167278390c77922f40175ce9bfea
juliusnjeru <juliusn411@gmail.com>
2023-07-22 10:11:13 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,135 @@
+# . /home/administrator/.local/share/virtualenvs/GENERAL_REPO-jpIBB4Np/bin/activate
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
+from taggit.models import Tag
+from django.db.models import Count
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
+def post_list(request, tag_slug=None):
+    # retrieve all published posts using our custom manager
+    post_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    # we want to add pagination to our views to render 4 items for each get query
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,
+            'tag': tag,
+        }
+    )
+
+
+def post_detail(request, year, month, day, postx):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=postx,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+
+    comments = post.comments.filter(active=True)
+
+    form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'form': form,
+            'comments': comments,
+            'similar_posts': similar_posts
+        }
+    )
+
+
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            # Send mail here.
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+                      [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request,
+                  'blog/post/comment.html',
+                  {
+                      'post': post,
+                      'form': form,
+                      'comment': comment
+                  })
